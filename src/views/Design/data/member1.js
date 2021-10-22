9659561ec5eaec89b664bfd0b20b0371a8c80b5e
Zack Tillotson <Zachery.Tillotson@CVSHealth.com>
2021-10-22 15:02:14 -0600

    Fixes design page data

--- src/views/Design/data/member1.js
+++ src/views/Design/data/member1.js
@@ -2,7 +2,7 @@
 import { Member, Club, Item, Acquisition, Session } from 'data/objectCreator'
 import { Ownership } from '../../../data/objectCreator'
 
-const item1 = new Item('1', {
+const item1 = new Item({id: '1', attributes: {
   name: 'Risk Legacy',
   releaseDate: '2019',
   publisher: 'Hasbro',
@@ -12,9 +12,9 @@ const item1 = new Item('1', {
   minPlayers: '1',
   maxPlayers: '5',
   price: '$70',
-})
+}})
 
-const item2 = new Item('2', {
+const item2 = new Item({id: '2', attributes: {
   canonicalImage: "https://cf.geekdo-images.com/vBOp95hH-UhrDawnMyY-xQ__original/img/5wEUzXvvsusT9Lxk6LXOZmgVHYY=/0x0/filters:format(png)/pic3673652.png",
   description: "The year is 3042: Humanity is ready to explore the galaxy. The most interesting celestial bodies to explore, and eventually colonize, have been known for centuries, and the nations of Earth finally have the technological level to reach them, thus beginning an unarmed competition that in the end the whole of humanity will win.\n\nKepler-3042 is a resource management game in which you have to explore, colonize, exploit, and terraform the planets of the Milky Way using the available technologies. In each round, you must choose which action to perform and which bonus to activate, managing your supplies of matter, energy and antimatter. The peculiar strength of the game is the innovative resource management: Each player has a finite amount of matter, energy and antimatter that they can produce or spend during the game. In each round, they can decide to burn forever one or more resources to perform powerful actions, thereby allowing them to follow different strategies.\n\n",
   designer: "Simone Cerruti Sola",
@@ -24,9 +24,9 @@ const item2 = new Item('2', {
   publisher: "Origames",
   releaseDate: "2016",
   price: ""
-})
+}})
 
-const item3 = new Item('3', {
+const item3 = new Item({id: '3', attributes: {
   canonicalImage: "https://cf.geekdo-images.com/aoPM07XzoceB-RydLh08zA__original/img/bYLzrwZO9u-3h2wyU-PHc8aTOY4=/0x0/filters:format(jpeg)/pic428828.jpg",
   description: "In Small World, players vie for conquest and control of a world that is simply too small to accommodate them all.&amp;#10;&amp;#10;Designed by Philippe Keyaerts as a fantasy follow-up to his award-winning Vinci, Small World is inhabited by a zany cast of characters such as dwarves, wizards, amazons, giants, orcs, and even humans, who use their troops to occupy territory and conquer adjacent lands in order to push the other races off the face of the earth.&amp;#10;&amp;#10;Picking the right combination from the 14 different fantasy races and 20 unique special powers, players rush to expand their empires - often at the expense of weaker neighbors. Yet they must also know when to push their own over-extended civilization into decline and ride a new one to victory!&amp;#10;&amp;#10;On each turn, you either use the multiple tiles of your chosen race (type of creatures) to occupy adjacent (normally) territories - possibly defeating weaker enemy races along the way, or you give up on your race letting it go &amp;quot;into decline&amp;quot;. A race in decline is designated by flipping the tiles over to their black-and-white side.&amp;#10;&amp;#10;At the end of your turn, you score one point (coin) for each territory your races occupy. You may have one active race and one race in decline on the board at the same time. Your occupation total can vary depending on the special abilities of your race and the territories they occupy. After the final round, the player with the most coins wins.&amp;#10;&amp;#10;Clarifications: available in a pinned forum post.&amp;#10;&amp;#10;",
   designer: "Philippe Keyaerts",
@@ -37,32 +37,32 @@ const item3 = new Item('3', {
   publisher: "Days of Wonder",
   releaseDate: "2009",
   price: "", 
-})
+}})
 
-const ownership1 = new Ownership(item1)
-ownership1.acquisitions.push(new Acquisition({ 
+const ownership1 = new Ownership({item: item1})
+ownership1.acquisitions.push(new Acquisition({attributes: {
   date: '2021-10-01',
   price: "$40",
-}))
-ownership1.acquisitions.push(new Acquisition({ 
+}}))
+ownership1.acquisitions.push(new Acquisition({attributes: { 
   date: '2020-03-20',
   price: "$50",
-}))
+}}))
 ownership1.attributes.favorite = true
 ownership1.attributes.collection = true
 
-const ownership2 = new Ownership(item2)
+const ownership2 = new Ownership({item: item2})
 ownership2.attributes.collection = true
 
-const ownership3 = new Ownership(item3)
+const ownership3 = new Ownership({item: item3})
 ownership3.attributes.collection = true
 ownership3.attributes.wishlist = true
 
-const member = new Member('member1', {name: 'Alice McGee'})
-const club = new Club({name: 'Test Club #1'})
+const member = new Member({attributes: {name: 'Alice McGee'}})
+const club = new Club({attributes: {name: 'Test Club #1'}})
 
-club.sessions.push(new Session(item1, {participants: ['Alice', 'Bob', 'Charlie', 'Dan', 'Erin']}))
-club.sessions.push(new Session(item2, {participants: ['Alice McGee', 'Bob Longnamington', 'Charlie The Man Dude', 'Dan', 'Erin Erinovicherinly']}))
+club.sessions.push(new Session({item: item1, attributes: {participants: ['Alice', 'Bob', 'Charlie', 'Dan', 'Erin']}}))
+club.sessions.push(new Session({item: item2, attributes: {participants: ['Alice McGee', 'Bob Longnamington', 'Charlie The Man Dude', 'Dan', 'Erin Erinovicherinly']}}))
 
 member.clubs.push(club)
 member.ownerships.push(ownership1, ownership2, ownership3)
