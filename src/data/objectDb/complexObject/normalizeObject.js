e65ac9b3c54e412dc7e14f6a80739a8a7a270555
Zack Tillotson <Zachery.Tillotson@CVSHealth.com>
2021-10-22 13:29:18 -0600

    Fixes dupcliate reference bug

--- src/data/objectDb/complexObject/normalizeObject.js
+++ src/data/objectDb/complexObject/normalizeObject.js
@@ -2,17 +2,18 @@ import walkObject, {IS_OBJ, IS_REF, NO_FILTER, setChildPath} from '../util/walkO
 import Ref from '../ref'
 
 function normalizeObject(target) {
-  const objects = [target]
+  const objects = new Map()
+  objects.set(target.ref, target)
 
   walkObject(target, child => {
-    objects.push(child)
+    objects.set(child.ref, child)
   }, {
     filter: IS_OBJ,
     recurse: true,
   })
 
   // We're modifying the parameter object. I think that's ok, but TBD
-  const normalObjs = objects.map(object => {
+  const normalObjs = [...objects.entries].map(object => {
     const cleanObject = {}
     walkObject(object, (child, path) => {
       const newValue = IS_OBJ(child) ? child.ref : child
